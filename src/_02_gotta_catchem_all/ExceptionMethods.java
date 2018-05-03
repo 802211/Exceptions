769c5df99f65452eeb8b1f1171066b7decee85c7
Chloe Luwa <chloeluwa@yahoo.com>
2018-05-02 18:29:28 -0700

    ExceptionsTest, ExceptionMethods
    ExceptionsTest, ExceptionMethods

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,25 @@
 package _02_gotta_catchem_all;
 
-public class ExceptionMethods {
-	
+public class ExceptionMethods extends Exception {
+
+	public double divide(double d1, double d2) throws IllegalArgumentException {
+		if (d2 == 0) {
+			throw new IllegalArgumentException();
+		}
+		double d1and2 = d1 / d2;
+		return d1and2;
+	}
+
+	public String reverseString(String s) throws IllegalStateException {
+		String revS = "";
+		if (s.length() == 0) {
+			throw new IllegalStateException("werty");
+		}
+		for (int i = s.length() - 1; i >= 0; i--) {
+			revS = revS + s.charAt(i);
+		}
+
+		return revS;
+	}
+
 }
